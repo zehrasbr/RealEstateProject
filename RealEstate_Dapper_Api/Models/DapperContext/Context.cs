f40450d19e9f2c819871822c48ff53b37490f2cb
zehra <zehraisbr@gmail.com>
2025-02-21 19:56:42 +0300

    sql

--- RealEstate_Dapper_Api/Models/DapperContext/Context.cs
+++ RealEstate_Dapper_Api/Models/DapperContext/Context.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.Data.SqlClient;
-using System.Data;
+﻿using System.Data;
+using System.Data.SqlClient;
 
 namespace RealEstate_Dapper_Api.Models.DapperContext
 {
     public class Context
     {
         private readonly IConfiguration _configuration;
-
         private readonly string _connectionString;
+
         public Context(IConfiguration configuration)
         {
-            _configuration=configuration;
-            _connectionString =_configuration.GetConnectionString("connection");
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("connection");
         }
         public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
 
